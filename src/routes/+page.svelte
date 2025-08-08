bd598721aae78f563b5d122a3f68194a31127a6c
sieugene <sieugene@mail.ru>
2025-08-08 22:21:19 +0900

    chore : test component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import TestComponent from '../shared/ui/TestComponent/TestComponent.svelte';
+</script>
+
+<h2>Test</h2>
+<TestComponent />
+
+<style>
+	h2 {
+		color: red;
+	}
+</style>
